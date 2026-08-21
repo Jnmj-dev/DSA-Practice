1457b2db7195a0786bf2fe94ae952e596dcab5e4
Janamjai <86583860+Jnmj-dev@users.noreply.github.com>
2026-08-22 01:56:39 +0530

    Refactor ReverseInteger.cpp to include main function

--- 01-Learn The Basics/03-Basic Maths/ReverseInteger.cpp
+++ 01-Learn The Basics/03-Basic Maths/ReverseInteger.cpp
@@ -1,14 +1,22 @@
-class Solution {
-public:
-    int reverse(int x) {
-        int ans = 0;
+#include <iostream>
+using namespace std;
 
-        while (x != 0) {
-            int digit = x % 10
-            x /= 10;
-
-            ans = ans * 10 + digit;
-        }
-        return ans;
+int reverseNumber(int n){
+    int ans = 0;
+    while (n != 0){
+        int digit = n % 10;
+        ans = ans * 10 + digit;
+        n = n / 10;
     }
-};
+    return ans;
+}
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Reversed number: " << reverseNumber(n) << endl;
+
+    return 0;
+}̥̥̥
